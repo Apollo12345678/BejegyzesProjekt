897f11bbeb8045538f071e4aab1608ace60e2d38
Petrik Lajos Szg <info@petrik.hu>
2018-10-01 15:57:40 +0200

    kesz munka

---
+++ BejegyzesProjekt/Bejegyzes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProjekt
+{
+    class Bejegyzes
+    {
+        private string szerzo;
+        private string tartalom;
+        private int likok;
+        private DateTime letrejott;
+        private DateTime szerkesztve;
+
+        public Bejegyzes(string szerzo, string tartalom)
+        {
+            this.szerzo = szerzo;
+            this.tartalom = tartalom;
+            this.letrejott = DateTime.Now;
+        }
+
+        public string getSzerzo() { return this.szerzo; }
+        public string getTartalom() { return this.tartalom; }
+        public void setTartalom(string tartalom)
+        {
+            this.tartalom = tartalom;
+        }
+        public int getLikok() { return this.likok; }
+        public void setLikekok(int value)
+        {
+            this.likok = value;
+        }
+        public DateTime getLetrejott() { return this.letrejott; }
+        public DateTime getSzerkesztve() {
+            if (this.tartalom != "")
+            {
+                return DateTime.Now;
+            }
+            else
+            {
+                return getLetrejott();
+            }
+        }       
+
+        public void Like()
+        {
+            this.likok++;
+        }
+
+        public string Kiir()
+        {
+            string valasz = this.szerzo + "-" + this.likok + "-" + this.letrejott + "\nSzerkesztve: " + getSzerkesztve() + "\n" + this.tartalom;
+            return valasz;
+        }
+    }
+}
---
+++ BejegyzesProjekt/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.feladat 
+            Bejegyzes h = new Bejegyzes("","");        
+            Console.WriteLine("valtoztass a tartalmon!");
+            h.setTartalom(Console.ReadLine());
+            Console.WriteLine(h.Kiir());
+
+            //2.feladat
+            List<Bejegyzes> bejegyzes = new List<Bejegyzes>();
+            bejegyzes.Add(new Bejegyzes("szemely2", "jegyzes2"));
+            bejegyzes.Add(new Bejegyzes("szemely3", "jegyzes3"));
+            int db;
+            Console.WriteLine("Adj meg egy szamot");
+            while(!(int.TryParse(Console.ReadLine(), out db)))
+            {
+                Console.WriteLine("Szamot adj meg");
+            }
+            int szam = 3;            
+            for(int i=0;i< db; i++)
+            {               
+                Console.WriteLine("Add meg a "+szam+++" bejegyzest: ");
+                bejegyzes.Add(new Bejegyzes(Console.ReadLine(), Console.ReadLine()));
+            }
+            int num = 20 * bejegyzes.Count;
+            Random ran = new Random();
+            foreach(var i in bejegyzes)
+            {
+                i.setLikekok(ran.Next(1,num+1));
+            }
+
+            foreach(var i in bejegyzes)
+            {
+                Console.WriteLine(i.Kiir());
+            }
+
+            //3.feladat
+            int max = 0;
+            foreach(var i in bejegyzes)
+            {
+                if (i.getLikok() > max)
+                {
+                    max = i.getLikok();
+                }
+            }
+            Console.WriteLine("\nLikok szama a legnepszerubb bejegyzesben: "+max);
+            Boolean vane = false;
+            foreach(var i in bejegyzes)
+            {
+                if (i.getLikok() > 35)
+                {
+                    vane = true;
+                }
+            }
+            if (vane)
+            {
+                Console.WriteLine("\nVan 35-nel tobb lakot tartalmazo bejegyzes");
+            }
+            else
+            {
+                Console.WriteLine("\nNincs ilyen 35-nel tobb likot tartalmozo bejegyzes");
+            }
+            int hany = 0;
+            foreach(var i in bejegyzes)
+            {
+                if (i.getLikok() < 15)
+                {
+                    hany++;
+                }
+            }
+            Console.WriteLine("\nEnnyi 15-nel kevesebb likot tartalmozo bejegyzes van: "+hany);
+            bejegyzes.Sort(delegate (Bejegyzes x, Bejegyzes y)
+            {
+                return y.getLikok().CompareTo(x.getLikok());
+            });
+            Console.WriteLine("\nLista likok szerint csokkenve:");
+            foreach (var i in bejegyzes)
+            {
+                Console.WriteLine(i.Kiir());
+            }
+            Console.ReadKey();
+        }
+    }
+}
